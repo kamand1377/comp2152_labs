416336b6d718663c506b1b362293e838db9c9c7b
kamand1377 <kamand.rostami@georgebrown.ca>
2026-02-04 22:08:02 -0500

    Lab 1

---
+++ lab1.py
@@ -0,0 +1,39 @@
+# Sample Coding Questions 01 Week 01
+# Kamand Rostami
+
+
+# -------------------------------
+# Defining Variables
+# -------------------------------
+numbers = [1, 4, 7, 9]
+
+
+# -------------------------------
+# Order of Operations
+# -------------------------------
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully-bracketed version of:
+# e = a - b ** c // d + a % c
+e = a - ((b ** c) // d) + (a % c)
+
+print("Value of e:", e)
+
+
+# -------------------------------
+# Formatting
+# -------------------------------
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+
+# -------------------------------
+# Common Functions
+# -------------------------------
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+
+print("Now showing the shop items filtered by age:", userAge)
