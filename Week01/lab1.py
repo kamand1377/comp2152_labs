82f77d53ef059ebe3dda193c7adddd8819d0a2b8
kamand1377 <kamand.rostami@georgebrown.ca>
2026-02-04 23:02:26 -0500

    add solution for week 02

--- Week01/lab1.py
+++ Week01/lab1.py
@@ -1,16 +1,10 @@
 # Sample Coding Questions 01 Week 01
 # Kamand Rostami
 
-
-# -------------------------------
 # Defining Variables
-# -------------------------------
 numbers = [1, 4, 7, 9]
 
-
-# -------------------------------
 # Order of Operations
-# -------------------------------
 a = 1
 b = 2
 c = 3
@@ -23,16 +17,11 @@ e = a - ((b ** c) // d) + (a % c)
 print("Value of e:", e)
 
 
-# -------------------------------
 # Formatting
-# -------------------------------
 temperature = 32.6
 print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
 
-
-# -------------------------------
 # Common Functions
-# -------------------------------
 userAge = int(input("Enter your age: "))
 userAge = userAge + 22
 
